c2862ca0681295945ebe5d11a9e6495d75f24152
kraskakraskova <118130412+kraskakraskova@users.noreply.github.com>
2023-02-23 18:30:27 +0400

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var pairOfBrackets = {};
+  var openingBrackets = [];
+  var similarBrackets = [];
+
+  bracketsConfig.forEach((element) => {
+    openingBrackets.push(element[0]);
+    pairOfBrackets[element[0]] = element[1];
+    if (element[0] === element[1]) {
+      similarBrackets.push(element[0]);
+    }
+  });
+
+  var stack = [];
+
+  for (i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+    if (openingBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+      if (
+        currentSymbol === topElement &&
+        similarBrackets.includes(currentSymbol)
+      ) {
+        stack.pop();
+        stack.pop();
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      if (pairOfBrackets[topElement] === currentSymbol) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
